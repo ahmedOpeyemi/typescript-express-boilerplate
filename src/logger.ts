029bd6f2a52b94f13aae31258bd088f29e3dc32b
Ahmed <ahmed.opeyemi@gmail.com>
2020-04-13 15:12:15 +0100

    Add winston logger

---
+++ src/logger.ts
@@ -0,0 +1,10 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console()
+    ]
+});
+
+export default logger;
